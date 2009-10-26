73f9beb1c3f156db360bfcc507295feeed4d38dd
Wesley Beary <wbeary@engineyard.com>
2009-10-25 23:41:55 -0700

    only using stdlib, so no need to require rubygems

--- lib/excon.rb
+++ lib/excon.rb
@@ -4,7 +4,6 @@ $LOAD_PATH.unshift __DIR__ unless
   $LOAD_PATH.include?(__DIR__) ||
   $LOAD_PATH.include?(File.expand_path(__DIR__))
 
-require 'rubygems'
 require 'openssl'
 require 'socket'
 require 'uri'
