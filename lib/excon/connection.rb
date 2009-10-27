d8b024b8dbbf24219aa5efa80d6e58114a3e9575
Wesley Beary <wbeary@engineyard.com>
2009-10-26 17:54:14 -0700

    bust connection cache on connection errors

--- lib/excon/connection.rb
+++ lib/excon/connection.rb
@@ -10,73 +10,78 @@ module Excon
     end
 
     def request(params)
-      params[:path] ||= ''
-      unless params[:path][0..0] == '/'
-        params[:path] = '/' + params[:path].to_s
-      end
-      if params[:query] && !params[:query].empty?
-        params[:path] << "?#{params[:query]}"
-      end
-      request = "#{params[:method]} #{params[:path]} HTTP/1.1\r\n"
-      params[:headers] ||= {}
-      params[:headers]['Host'] = params[:host] || @uri.host
-      if params[:body] && !params[:headers]['Content-Length']
-        params[:headers]['Content-Length'] = params[:body].length
-      end
-      for key, value in params[:headers]
-        request << "#{key}: #{value}\r\n"
-      end
-      request << "\r\n"
-      connection.write(request)
-
-      if params[:body]
-        if params[:body].is_a?(String)
-          connection.write(params[:body])
-        else
-          while chunk = params[:body].read(CHUNK_SIZE)
-            connection.write(chunk)
-          end
+      begin
+        params[:path] ||= ''
+        unless params[:path][0..0] == '/'
+          params[:path] = "/#{params[:path]}"
         end
-      end
-
-      response = Excon::Response.new
-      response.status = connection.readline[9..11].to_i
-      while true
-        data = connection.readline.chop!
-        unless data.empty?
-          header = data.split(': ')
-          response.headers[header[0]] = header[1]
-        else
-          break
+        if params[:query] && !params[:query].empty?
+          params[:path] << "?#{params[:query]}"
         end
-      end
-
-      unless params[:method] == 'HEAD'
-        unless params[:block]
-          response.body = ''
-          params[:block] = lambda { |chunk| response.body << chunk }
+        request = "#{params[:method]} #{params[:path]} HTTP/1.1\r\n"
+        params[:headers] ||= {}
+        params[:headers]['Host'] = params[:host] || @uri.host
+        if params[:body] && !params[:headers]['Content-Length']
+          params[:headers]['Content-Length'] = params[:body].length
         end
+        for key, value in params[:headers]
+          request << "#{key}: #{value}\r\n"
+        end
+        request << "\r\n"
+        connection.write(request)
 
-        if response.headers['Content-Length']
-          remaining = response.headers['Content-Length'].to_i
-          while remaining > 0
-            params[:block].call(connection.read([CHUNK_SIZE, remaining].min))
-            remaining -= CHUNK_SIZE
-          end
-        elsif response.headers['Transfer-Encoding'] == 'chunked'
-          while true
-            chunk_size = connection.readline.chomp!.to_i(16)
-            chunk = connection.read(chunk_size + 2).chop! # 2 == "/r/n".length
-            if chunk_size > 0
-              params[:block].call(chunk)
-            else
-              break
+        if params[:body]
+          if params[:body].is_a?(String)
+            connection.write(params[:body])
+          else
+            while chunk = params[:body].read(CHUNK_SIZE)
+              connection.write(chunk)
             end
           end
-        elsif response.headers['Connection'] == 'close'
-          params[:block].call(connection.read)
-          @connection = nil
         end
+
+        response = Excon::Response.new
+        response.status = connection.readline[9..11].to_i
+        while true
+          data = connection.readline.chop!
+          unless data.empty?
+            header = data.split(': ')
+            response.headers[header[0]] = header[1]
+          else
+            break
+          end
+        end
+
+        unless params[:method] == 'HEAD'
+          unless params[:block]
+            response.body = ''
+            params[:block] = lambda { |chunk| response.body << chunk }
+          end
+
+          if response.headers['Content-Length']
+            remaining = response.headers['Content-Length'].to_i
+            while remaining > 0
+              params[:block].call(connection.read([CHUNK_SIZE, remaining].min))
+              remaining -= CHUNK_SIZE
+            end
+          elsif response.headers['Transfer-Encoding'] == 'chunked'
+            while true
+              chunk_size = connection.readline.chomp!.to_i(16)
+              chunk = connection.read(chunk_size + 2).chop! # 2 == "/r/n".length
+              if chunk_size > 0
+                params[:block].call(chunk)
+              else
+                break
+              end
+            end
+          elsif response.headers['Connection'] == 'close'
+            params[:block].call(connection.read)
+            @connection = nil
+          end
+        end
+      rescue => connection_error
+        @connection = nil
+        raise(connection_error)
       end
 
       if params[:expects] && ![*params[:expects]].include?(response.status)
