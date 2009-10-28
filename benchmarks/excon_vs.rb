909f11508f01654d4fb6de661ed2158dba1fc7c8
Wesley Beary <wbeary@engineyard.com>
2009-10-27 22:35:59 -0700

    more benchmarks

--- benchmarks/excon_vs.rb
+++ benchmarks/excon_vs.rb
@@ -1,9 +1,10 @@
 require File.join(File.dirname(__FILE__), '..', 'lib/excon')
 
 require 'benchmark'
+require 'net/http'
 require 'open-uri'
 
-COUNT = 10
+COUNT = 100
 data = "Content-Length: 100"
 Benchmark.bmbm(25) do |bench|
   bench.report('excon') do
@@ -15,6 +16,19 @@ Benchmark.bmbm(25) do |bench|
     excon = Excon.new('http://www.google.com')
     COUNT.times do
       excon.request(:method => 'GET', :path => '/')
+    end
+  end
+  bench.report('net/http') do
+    COUNT.times do
+      # Net::HTTP.get('www.google.com', '/')
+      Net::HTTP.start('www.google.com') {|http| http.get('/') }
+    end
+  end
+  bench.report('net/http (persistent)') do
+    Net::HTTP.start('www.google.com', 80) do |http|
+      COUNT.times do
+        http.get('/')
+      end
     end
   end
   bench.report('open-uri') do
