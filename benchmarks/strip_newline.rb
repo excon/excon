d06f06bada707f48958e6c882666a165ad0fa76f
geemus (Wesley Beary) <wbeary@engineyard.com>
2010-04-09 19:20:08 -0700

    playing with tach

--- benchmarks/strip_newline.rb
+++ benchmarks/strip_newline.rb
@@ -1,51 +1,51 @@
-require 'benchmark'
-
-COUNT = 1_000_000
-data = "Content-Length: 100\r\n"
-Benchmark.bmbm(25) do |bench|
-  bench.report('chomp') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.chomp
-    end
-  end
-  bench.report('chomp!') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.chomp!
-    end
-  end
-  bench.report('chop') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.chop
-    end
-  end
-  bench.report('chop!') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.chop!
-    end
-  end
-  bench.report('strip') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.strip
-    end
-  end
-  bench.report('strip!') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data.strip!
-    end
-  end
-  bench.report('index') do
-    COUNT.times do
-      data = "Content-Length: 100\r\n"
-      data[0..-3]
-    end
-  end
-end
+# require 'benchmark'
+# 
+# COUNT = 1_000_000
+# data = "Content-Length: 100\r\n"
+# Benchmark.bmbm(25) do |bench|
+#   bench.report('chomp') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.chomp
+#     end
+#   end
+#   bench.report('chomp!') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.chomp!
+#     end
+#   end
+#   bench.report('chop') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.chop
+#     end
+#   end
+#   bench.report('chop!') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.chop!
+#     end
+#   end
+#   bench.report('strip') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.strip
+#     end
+#   end
+#   bench.report('strip!') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data.strip!
+#     end
+#   end
+#   bench.report('index') do
+#     COUNT.times do
+#       data = "Content-Length: 100\r\n"
+#       data[0..-3]
+#     end
+#   end
+# end
 
 
 
@@ -67,3 +67,49 @@ end
 # strip                      0.640000   0.000000   0.640000 (  0.646328)
 # strip!                     0.520000   0.000000   0.520000 (  0.532715)
 # index                      0.740000   0.010000   0.750000 (  0.771277)
+
+require 'rubygems'
+require 'tach'
+
+data = "Content-Length: 100\r\n"
+Tach.meter(1_000_000) do
+  tach('chomp') do
+    data.dup.chomp
+  end
+  tach('chomp!') do
+    data.dup.chomp!
+  end
+  tach('chop') do
+    data.dup.chop
+  end
+  tach('chop!') do
+    data.dup.chop!
+  end
+  tach('strip') do
+    data.dup.strip
+  end
+  tach('strip!') do
+    data.dup.strip!
+  end
+  tach('index') do
+    data.dup[0..-3]
+  end
+end
+
+# +--------+----------+----------+
+# | tach   | average  | total    |
+# +--------+----------+----------+
+# | chomp  | 1.444547 | 1.444547 |
+# +--------+----------+----------+
+# | chomp! | 1.276813 | 1.276813 |
+# +--------+----------+----------+
+# | chop   | 1.422744 | 1.422744 |
+# +--------+----------+----------+
+# | chop!  | 1.240941 | 1.240941 |
+# +--------+----------+----------+
+# | strip  | 1.444776 | 1.444776 |
+# +--------+----------+----------+
+# | strip! | 1.266459 | 1.266459 |
+# +--------+----------+----------+
+# | index  | 1.557975 | 1.557975 |
+# +--------+----------+----------+
