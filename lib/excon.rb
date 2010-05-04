705237bbc509dee37a747f2c43d98719bf212816
geemus <geemus@gmail.com>
2010-05-03 21:22:22 -0700

    setup rakegem

--- lib/excon.rb
+++ lib/excon.rb
@@ -14,6 +14,10 @@ require 'excon/response'
 
 module Excon
 
+  unless const_defined?(:VERSION)
+    VERSION = '0.0.18'
+  end
+
   CHUNK_SIZE = 1048576 # 1 megabyte
 
   def self.new(url, params = {})
