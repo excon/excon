07822125700ba867171ff8dfad434434b85125b4
Matt Sanders <matt@modal.org>
2011-04-14 16:12:32 -0500

    Remove commented tests.

--- tests/header_tests.rb
+++ tests/header_tests.rb
@@ -41,27 +41,7 @@ Shindo.tests('Excon response header support') do
         end
         
       end
-
-      # tests('response.status').returns(200) do
-      #   response.status
-      # end
-      # 
-      # # must be absolute form for proxy requests
-      # tests('sent Request URI').returns('http://foo.com:8080/bar?alpha=kappa') do
-      #   response.headers['Sent-Request-Uri']
-      # end
-      # 
-      # tests('sent Host header').returns('foo.com:8080') do
-      #   response.headers['Sent-Host']
-      # end
-      # 
-      # tests('sent Proxy-Connection header').returns('Keep-Alive') do
-      #   response.headers['Sent-Proxy-Connection']
-      # end
-      # 
-      # tests('response.body (proxied content)').returns('proxied content') do
-      #   response.body
-      # end
+      
     end
 
   end
