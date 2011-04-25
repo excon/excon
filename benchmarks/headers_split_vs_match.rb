43bd4db75d02dc583791aea207364ce10647aa08
geemus <geemus@gmail.com>
2011-04-25 12:34:09 -0700

    add benchmark for new header splitting

--- benchmarks/headers_split_vs_match.rb
+++ benchmarks/headers_split_vs_match.rb
@@ -1,29 +1,3 @@
-# require 'benchmark'
-
-# COUNT = 1_000_000
-# data = "Content-Length: 100"
-# Benchmark.bmbm(25) do |bench|
-#   bench.report('regex') do
-#     COUNT.times do
-#       header = data.match(/(.*):\s(.*)/)
-#     end
-#   end
-#   bench.report('split') do
-#     COUNT.times do
-#       header = data.split(': ')
-#     end
-#   end
-# end
-
-# Rehearsal ------------------------------------------------------------
-# regex                      4.270000   0.010000   4.280000 (  4.294186)
-# split                      3.870000   0.000000   3.870000 (  3.885645)
-# --------------------------------------------------- total: 8.150000sec
-# 
-#                                user     system      total        real
-# regex                      4.260000   0.010000   4.270000 (  4.284764)
-# split                      3.860000   0.010000   3.870000 (  3.882795)
-
 require 'rubygems'
 require 'tach'
 
@@ -34,9 +8,22 @@ Tach.meter(1_000_000) do
     header = [$1, $2]
   end
   tach('split') do
-    header = data.split(': ')
+    header = data.split(': ', 2)
+  end
+  tach('split regex') do
+    header = data.split(/:\s*/, 2)
   end
 end
+
+#  +-------------+----------+
+#  | tach        | total    |
+#  +-------------+----------+
+#  | split regex | 5.940233 |
+#  +-------------+----------+
+#  | split       | 7.327549 |
+#  +-------------+----------+
+#  | regex       | 8.736390 |
+#  +-------------+----------+
 
 # +-------+----------+----------+
 # | tach  | average  | total    |
