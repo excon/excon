707b41b99916906457eb33810e44e3f4ceac488a
Matt Sanders <matt@modal.org>
2011-10-04 22:44:28 -0500

    Purge extra whitespace.

--- tests/header_tests.rb
+++ tests/header_tests.rb
@@ -7,41 +7,41 @@ Shindo.tests('Excon response header support') do
       response = connection.request(:method => :get, :path => '/foo')
 
       tests('with variable header capitalization') do
-        
+
         tests('response.get_header("mixedcase-header")').returns('MixedCase') do
           response.get_header("mixedcase-header")
         end
-        
+
         tests('response.get_header("uppercase-header")').returns('UPPERCASE') do
           response.get_header("uppercase-header")
         end
-        
+
         tests('response.get_header("lowercase-header")').returns('lowercase') do
           response.get_header("lowercase-header")
         end
-        
+
       end
-      
+
       tests('when provided key capitalization varies') do
-        
+
         tests('response.get_header("MIXEDCASE-HEADER")').returns('MixedCase') do
           response.get_header("MIXEDCASE-HEADER")
         end
-        
+
         tests('response.get_header("MiXeDcAsE-hEaDeR")').returns('MixedCase') do
           response.get_header("MiXeDcAsE-hEaDeR")
         end
-        
+
       end
-      
+
       tests('when header is unavailable') do
-        
+
         tests('response.get_header("missing")').returns(nil) do
           response.get_header("missing")
         end
-        
+
       end
-      
+
     end
 
   end
