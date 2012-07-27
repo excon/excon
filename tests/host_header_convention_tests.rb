24f60a5b08267705d66886b949dfd78ea86cf4ef
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-07-27 19:11:56 +0200

    Refactor tests

--- tests/host_header_convention_tests.rb
+++ tests/host_header_convention_tests.rb
@@ -1,25 +1,25 @@
 Shindo.tests('Excon host header convention') do
 
   before do
-    Excon.stub({:method => :get}) { |params|
+    Excon.stub({}) { |params|
       {:body => params[:headers]['Host']}
     }
   end
 
   tests("An HTTP URL with a default port includes the port").returns("foo") do
-    Excon.new('http://foo:80', :mock => true).request(:method => :get).body
+    Excon.new('http://foo:80', :mock => true).get.body
   end
 
   tests("An HTTP URL with a non-default port includes the port").returns("foo:9292") do
-    Excon.new('http://foo:9292', :mock => true).request(:method => :get).body
+    Excon.new('http://foo:9292', :mock => true).get.body
   end
 
-  tests("An HTTPS URL with a non-default port includes the port").returns("foo") do
-    Excon.new('https://foo:443', :mock => true).request(:method => :get).body
+  tests("An HTTPS URL with a default port includes the port").returns("foo") do
+    Excon.new('https://foo:443', :mock => true).get.body
   end
 
   tests("An HTTPS URL with a non-default port includes the port").returns("foo:9443") do
-    Excon.new('https://foo:9443', :mock => true).request(:method => :get).body
+    Excon.new('https://foo:9443', :mock => true).get.body
   end
 
   after do
