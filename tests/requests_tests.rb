a1acf6af40bcfe793c7d8a001d1c950755dd05e2
geemus <geemus@gmail.com>
2013-02-03 17:07:53 -0600

    add REALLY basic pipeline test

---
+++ tests/requests_tests.rb
@@ -0,0 +1,31 @@
+with_rackup('basic.ru') do
+  Shindo.tests('requests should succeed') do
+
+    connection = Excon.new('http://127.0.0.1:9292')
+
+    tests('HEAD /content-length/100, GET /content-length/100') do
+      responses = connection.requests([
+        {:method => :head, :path => '/content-length/100'},
+        {:method => :get, :path => '/content-length/100'}
+      ])
+
+      tests('head body is empty').returns('') do
+        responses.first.body
+      end
+
+      tests('head content length is 100').returns('100') do
+        responses.first.headers['Content-Length']
+      end
+
+      tests('get body is non-empty').returns('x' * 100) do
+        responses.last.body
+      end
+
+      tests('get content length is 100').returns('100') do
+        responses.last.headers['Content-Length']
+      end
+
+    end
+
+  end
+end
