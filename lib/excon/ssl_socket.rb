a790ba437ed8fddd72997dc4e93f5782aa53bdf2
geemus <geemus@gmail.com>
2013-07-18 15:51:48 -0500

    simplify by ssl ca defaults
    still attempt to add default paths, but always add bundled
    I believe bundled should only be used as a fallback then
    which should match with desire and more generally 'just work'
    
    if this fails we may have to explore something like this:
    https://github.com/google/signet/blob/master/lib/signet/ssl_config.rb

--- lib/excon/ssl_socket.rb
+++ lib/excon/ssl_socket.rb
@@ -20,11 +20,8 @@ module Excon
           ssl_context.ca_file = ca_file
         else # attempt default, fallback to bundled
           ssl_context.cert_store = OpenSSL::X509::Store.new
-          if !defined?(OpenSSL::Config::DEFAULT_CONFIG_FILE) || File.exists?(OpenSSL::Config::DEFAULT_CONFIG_FILE)
-            ssl_context.cert_store.set_default_paths
-          else
-            ssl_context.cert_store.add_file(DEFAULT_CA_FILE)
-          end
+          ssl_context.cert_store.set_default_paths
+          ssl_context.cert_store.add_file(DEFAULT_CA_FILE)
         end
       else
         # turn verification off
