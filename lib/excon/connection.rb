320bd74b43da43e44ab2e39b40746c14f1f1a9b6
James Bence <jbence@mdsol.com>
2013-09-09 17:06:54 -0700

    Guard against nil exception

--- lib/excon/connection.rb
+++ lib/excon/connection.rb
@@ -317,7 +317,7 @@ module Excon
     private
 
     def invoke_response_block(datum)
-      if datum.has_key?(:response_block) && !datum[:response][:body].empty?
+      if datum.has_key?(:response_block) && datum[:response] && !datum[:response][:body].empty?
         content_length = remaining = datum[:response][:body].bytesize
         while remaining > 0
           datum[:response_block].call(datum[:response][:body].slice!(0, [datum[:chunk_size], remaining].min), [remaining - datum[:chunk_size], 0].max, content_length)
