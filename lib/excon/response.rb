258b088d8b5864a207e865ee07e9b7a84ae8124c
geemus <geemus@gmail.com>
2014-07-29 16:08:20 -0500

    assume socket will have remote/local info defined

--- lib/excon/response.rb
+++ lib/excon/response.rb
@@ -44,9 +44,9 @@ module Excon
         :body          => '',
         :headers       => Excon::Headers.new,
         :status        => status,
-        :remote_ip     => socket.respond_to?(:remote_ip) && socket.remote_ip,
-        :local_port    => socket.respond_to?(:local_port) && socket.local_port,
-        :local_address => socket.respond_to?(:local_address) && socket.local_address
+        :remote_ip     => socket.remote_ip,
+        :local_port    => socket.local_port,
+        :local_address => socket.local_address
       }
 
       parse_headers(socket, datum)
