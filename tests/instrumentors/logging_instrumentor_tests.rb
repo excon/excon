bbeb35af1218e29c27190b66830ab5eae6333e94
Christoph Rieß <riess@carlfinance.de>
2021-09-02 16:03:49 +0200

    [fix] logging behavior for query hashes
      * we must log only the query when testing for the query as hash
      * see modified behavior inside Excon::Instrumentors::LoggingInstrumentor
      * added very simple test

--- tests/instrumentors/logging_instrumentor_tests.rb
+++ tests/instrumentors/logging_instrumentor_tests.rb
@@ -26,6 +26,33 @@ Shindo.tests('logging instrumentor') do
     ]
   end
 
+  tests("connection logger with query as hash").returns(true) do
+    Excon.stub({:method => :get}, {body: 'body'}, status: 200})
+
+    log_path = "/tmp/excon_#{Time.now.to_i}.txt"
+    logger = Logger.new(log_path)
+    # omit datetime to simplify test matcher
+    logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{msg}\n"
+    end
+
+    connection = Excon.new(
+      'http://127.0.0.1:9292',
+      instrumentor: Excon::LoggingInstrumentor,
+      logger: logger,
+      mock: true
+    )
+    response = connection.request(
+      method: :get,
+      path: '/logger',
+      query: {test: 'test'}
+    )
+    File.readlines(log_path)[1..2] == [
+      "request: http://127.0.0.1/logger?test=test\n",
+      "response: body\n"
+    ]
+  end
+
   Excon.stubs.clear
   env_restore
 end
