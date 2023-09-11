9f9fc8da9c7b411b66b775b2128193538d506407
Bruno Martins <25222614+bruno-b-martins@users.noreply.github.com>
2023-09-11 16:55:42 +0000

    Make sinatra nameserver run from the spec

--- spec/requests/dns_timeout_spec.rb
+++ spec/requests/dns_timeout_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Excon::Socket do
+  include_context('test server', :webrick, 'dns_timeout.ru', before: :start, after: :stop)
+
   context 'when the DNS server takes too long to resolve' do
     let(:dns_resolver) { Resolv::DNS.new(nameserver_port: [['127.0.0.1', 4567]]) }
 
