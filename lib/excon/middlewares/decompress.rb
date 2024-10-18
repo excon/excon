74c38a3f5c6329e624233f8b0a6e64d20c31bf89
geemus <geemus@gmail.com>
2024-10-18 13:30:38 +0000

    style/cops for decompress middleware

--- lib/excon/middlewares/decompress.rb
+++ lib/excon/middlewares/decompress.rb
@@ -1,41 +1,36 @@
 # frozen_string_literal: true
+
 module Excon
   module Middleware
     class Decompress < Excon::Middleware::Base
-
       INFLATE_ZLIB_OR_GZIP  = 47 # Zlib::MAX_WBITS + 32
       INFLATE_RAW           = -15 # Zlib::MAX_WBITS * -1
 
       def request_call(datum)
-        unless datum.has_key?(:response_block)
-          key = datum[:headers].keys.detect {|k| k.to_s.casecmp('Accept-Encoding') == 0 } || 'Accept-Encoding'
-          if datum[:headers][key].to_s.empty?
-            datum[:headers][key] = 'deflate, gzip'
-          end
+        unless datum.key?(:response_block)
+          key = datum[:headers].keys.detect { |k| k.to_s.casecmp('Accept-Encoding').zero? } || 'Accept-Encoding'
+          datum[:headers][key] = 'deflate, gzip' if datum[:headers][key].to_s.empty?
         end
         @stack.request_call(datum)
       end
 
       def response_call(datum)
         body = datum[:response][:body]
-        unless datum.has_key?(:response_block) || body.nil? || body.empty?
-          if (key = datum[:response][:headers].keys.detect {|k| k.casecmp('Content-Encoding') == 0 })
-            encodings = Utils.split_header_value(datum[:response][:headers][key])
-            if (encoding = encodings.last)
-              if encoding.casecmp('deflate') == 0
-                datum[:response][:body] = begin
-                  Zlib::Inflate.new(INFLATE_ZLIB_OR_GZIP).inflate(body)
-                rescue Zlib::DataError # fallback to raw on error
-                  Zlib::Inflate.new(INFLATE_RAW).inflate(body)
-                end
-                encodings.pop
-              elsif encoding.casecmp('gzip') == 0 || encoding.casecmp('x-gzip') == 0
-                datum[:response][:body] = Zlib::GzipReader.new(StringIO.new(body)).read
-                encodings.pop
-              end
-              datum[:response][:headers][key] = encodings.join(', ')
+        if !(datum.key?(:response_block) || body.nil? || body.empty?) &&
+           (key = datum[:response][:headers].keys.detect { |k| k.casecmp('Content-Encoding').zero? })
+          encodings = Utils.split_header_value(datum[:response][:headers][key])
+          if encodings.last.casecmp('deflate').zero?
+            datum[:response][:body] = begin
+              Zlib::Inflate.new(INFLATE_ZLIB_OR_GZIP).inflate(body)
+            rescue Zlib::DataError # fallback to raw on error
+              Zlib::Inflate.new(INFLATE_RAW).inflate(body)
             end
+            encodings.pop
+          elsif encodings.last.casecmp('gzip').zero? || encodings.last.casecmp('x-gzip').zero?
+            datum[:response][:body] = Zlib::GzipReader.new(StringIO.new(body)).read
+            encodings.pop
           end
+          datum[:response][:headers][key] = encodings.join(', ')
         end
         @stack.response_call(datum)
       end
