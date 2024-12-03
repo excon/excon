cffb35c1f7321ad29f9218a4ec5feaee0e72e8d3
geemus <geemus@gmail.com>
2024-12-03 22:33:44 +0000

    manual progress on cops

--- lib/excon/utils.rb
+++ lib/excon/utils.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
+
 module Excon
   module Utils
     extend self
 
-    CONTROL   = (0x0..0x1f).map {|c| c.chr }.join + "\x7f"
+    CONTROL   = (0x0..0x1f).map(&:chr).join + "\x7f"
     DELIMS    = '<>#%"'
     UNWISE    = '{}|\\^[]`'
-    NONASCII  = (0x80..0xff).map {|c| c.chr }.join
+    NONASCII  = (0x80..0xff).map(&:chr).join
     UNESCAPED = /([#{ Regexp.escape(CONTROL + ' ' + DELIMS + UNWISE + NONASCII) }])/
-    ESCAPED   = /%([0-9a-fA-F]{2})/
+    ESCAPED   = /%([0-9a-fA-F]{2})/.freeze
 
     def binary_encode(string)
       if FORCE_ENC && string.encoding != Encoding::ASCII_8BIT
@@ -26,6 +27,7 @@ module Excon
       unless datum
         raise ArgumentError, '`datum` must be given unless called on a Connection'
       end
+
       if datum[:scheme] == UNIX
         "#{datum[:scheme]}://#{datum[:socket]}"
       else
@@ -36,21 +38,21 @@ module Excon
     # Redact sensitive info from provided data
     def redact(datum)
       datum = datum.dup
-      if datum.has_key?(:headers)
-        if datum[:headers].has_key?('Authorization') || datum[:headers].has_key?('Proxy-Authorization')
+      if datum.key?(:headers)
+        if datum[:headers].key?('Authorization') || datum[:headers].key?('Proxy-Authorization')
           datum[:headers] = datum[:headers].dup
         end
-        if datum[:headers].has_key?('Authorization')
+        if datum[:headers].key?('Authorization')
           datum[:headers]['Authorization'] = REDACTED
         end
-        if datum[:headers].has_key?('Proxy-Authorization')
+        if datum[:headers].key?('Proxy-Authorization')
           datum[:headers]['Proxy-Authorization'] = REDACTED
         end
       end
-      if datum.has_key?(:password)
+      if datum.key?(:password)
         datum[:password] = REDACTED
       end
-      if datum.has_key?(:proxy) && datum[:proxy] && datum[:proxy].has_key?(:password)
+      if datum.key?(:proxy) && datum[:proxy]&.key?(:password)
         datum[:proxy] = datum[:proxy].dup
         datum[:proxy][:password] = REDACTED
       end
@@ -76,13 +78,13 @@ module Excon
     end
 
     def query_string(datum)
-      str = String.new
+      str = +''
       case datum[:query]
       when String
         str << '?' << datum[:query]
       when Hash
         str << '?'
-        datum[:query].sort_by {|k,_| k.to_s }.each do |key, values|
+        datum[:query].sort_by { |k, _| k.to_s }.each do |key, values|
           key = CGI.escape(key.to_s)
           if values.nil?
             str << key << '&'
@@ -100,6 +102,7 @@ module Excon
     # Splits a header value +str+ according to HTTP specification.
     def split_header_value(str)
       return [] if str.nil?
+
       str = str.dup.strip
       str = binary_encode(str)
       str.scan(%r'\G((?:"(?:\\.|[^"])+?"|[^",])+)
@@ -110,36 +113,38 @@ module Excon
     def escape_uri(str)
       str = str.dup
       str = binary_encode(str)
-      str.gsub(UNESCAPED) { "%%%02X" % $1[0].ord }
+      str.gsub(UNESCAPED) { "%%%02X" % ::Regexp.last_match(1)[0].ord }
     end
 
     # Unescapes HTTP reserved and unwise characters in +str+
     def unescape_uri(str)
       str = str.dup
       str = binary_encode(str)
-      str.gsub(ESCAPED) { $1.hex.chr }
+      str.gsub(ESCAPED) { ::Regexp.last_match(1).hex.chr }
     end
 
     # Unescape form encoded values in +str+
     def unescape_form(str)
       str = str.dup
       str = binary_encode(str)
-      str.gsub!(/\+/, ' ')
-      str.gsub(ESCAPED) { $1.hex.chr }
+      str.tr!('+', ' ')
+      str.gsub(ESCAPED) { ::Regexp.last_match(1).hex.chr }
     end
 
     # Performs validation on the passed header hash and returns a string representation of the headers
     def headers_hash_to_s(headers)
-      headers_str = String.new
+      headers_str = +''
       headers.each do |key, values|
-        if key.to_s.match(/[\r\n]/)
+        if key.to_s.match?(/[\r\n]/)
           raise Excon::Errors::InvalidHeaderKey.new(key.to_s.inspect + ' contains forbidden "\r" or "\n"')
         end
+
         [values].flatten.each do |value|
-          if value.to_s.match(/[\r\n]/)
+          if value.to_s.match?(/[\r\n]/)
             # Don't include the potentially sensitive header value (i.e. authorization token) in the message
             raise Excon::Errors::InvalidHeaderValue.new(key.to_s + ' header value contains forbidden "\r" or "\n"')
           end
+
           headers_str << key.to_s << ': ' << value.to_s << CR_NL
         end
       end
